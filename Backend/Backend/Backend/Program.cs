bbeb7e95a03c1c1f25748158cf94fc143dc7fbc7
DarkTImes <darktimesareuponus@gmail.com>
2019-05-06 18:14:16 +0100

    Initial commit of backend

---
+++ Backend/Backend/Backend/Program.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Classes;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://*:2609/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+                Console.ReadLine();
+            }
+        }
+    }
+}
